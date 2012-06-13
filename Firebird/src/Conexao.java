06c52cf80ded2e4ede6fb6b0915416cd032b9c13
EricBorba <eric.borba@gmail.com>
2012-06-13 12:05:27 -0300

    comecando o projeto Firebird

---
+++ Firebird/src/Conexao.java
@@ -0,0 +1,122 @@
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import org.firebirdsql.gds.TransactionParameterBuffer;
+import org.firebirdsql.jdbc.FBConnection;
+import org.firebirdsql.jdbc.FirebirdConnection;
+
+public class Conexao {
+
+	static String url;
+	static String drive;
+	static String login; 
+	static String pass;
+	static FirebirdConnection connection;
+
+
+	/**
+	 * @param args
+	 * @throws SQLException 
+	 * @throws ClassNotFoundException 
+	 */
+	public static void main(String[] args)  {
+
+		url = "jdbc:firebirdsql:localhost/3050:/Library/Frameworks/Firebird.framework/Flycin.fdb" ;                                                                                                                 
+		drive = "org.firebirdsql.jdbc.FBDriver";
+		login = "SYSDBA";
+		pass = "masterkey";
+		connection = null;
+	}
+	
+	public Conexao(){
+
+		try {
+			Class.forName(drive);
+			System.out.println("Drive Carregado");
+		} catch (ClassNotFoundException e1) {
+			System.out.println("n conseguiu carregar drive");
+		}
+
+
+
+		try {
+
+			connection = (FBConnection) DriverManager.getConnection( url, login, pass);
+
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+
+
+	}
+
+	public FirebirdConnection leituraInicial(){
+
+
+		try {
+			
+			connection.setAutoCommit(false);
+
+			System.out.println("Ok");
+
+			System.out.print("Passando os parametros de conexao...");
+			
+			TransactionParameterBuffer tp = connection.createTransactionParameterBuffer();
+			tp.addArgument(FirebirdConnection.TPB_READ_COMMITTED);
+			tp.addArgument(FirebirdConnection.TPB_READ);
+			tp.addArgument(FirebirdConnection.TPB_REC_VERSION);
+			connection.setTransactionParameters(tp);
+			
+			System.out.println("Ok");
+
+
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return connection;
+
+
+	}
+	
+	public FirebirdConnection selecionandoAssento(){
+
+
+		try {
+			
+			connection.setAutoCommit(false);
+
+			System.out.println("Ok");
+
+			System.out.print("Passando os parametros de conexao...");
+			
+			TransactionParameterBuffer tp = connection.createTransactionParameterBuffer();
+			tp.addArgument(FirebirdConnection.TPB_READ_COMMITTED);
+			tp.addArgument(FirebirdConnection.TPB_WRITE);
+			tp.addArgument(FirebirdConnection.TPB_NO_REC_VERSION);
+			tp.addArgument(FirebirdConnection.TPB_NOWAIT);
+			connection.setTransactionParameters(tp);
+			
+			System.out.println("Ok");
+
+
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			System.out.println("ja esta selecionado");
+		}
+
+		return connection;
+
+
+	}
+	
+	
+
+
+}
+
+
