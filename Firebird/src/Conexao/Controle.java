8132b98574fb9050ec174866782ce30578844cf0
EricBorba <eric.borba@gmail.com>
2012-06-13 21:12:47 -0300

    mais metodos

--- Firebird/src/Conexao/Controle.java
+++ Firebird/src/Conexao/Controle.java
@@ -3,6 +3,7 @@ package Conexao;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 import org.firebirdsql.jdbc.FirebirdConnection;
 
@@ -17,9 +18,10 @@ public class Controle {
 		boolean existePassageiro = false;
 
 
-		conexao.setAutoCommit(false);
-
-		PreparedStatement  statement = conexao.prepareStatement("SELECT p.cpf FROM PASSAGEIRO p WHERE p.cpf = ? AND p.senha = ?");
+		//conexao.setAutoCommit(false);
+		
+		// Esse lock eh justificado apenas pra garantir uma unica instancia do sistema logada ( nao ter 2 sistemas logados com o mesmo usuario )
+		PreparedStatement  statement = conexao.prepareStatement("SELECT p.cpf FROM PASSAGEIRO p WHERE p.cpf = ? AND p.senha = ? WITH LOCK");
 		statement.setInt(1, cpf);
 		statement.setInt(2, senha);
 
@@ -43,19 +45,45 @@ public class Controle {
 
 	}
 
-	public void cadastrarPassageiro(FirebirdConnection conexao, int cpf, String nome, String endereco, int senha){
+	public void cadastrarPassageiro(FirebirdConnection conexao, int cpf, String nome, String endereco, int senha) throws SQLException{
 
+		//conexao.setAutoCommit(false);
+
+		PreparedStatement  statement = conexao.prepareStatement("INSERT INTO PASSAGEIRO (CPF, NOME, ENDERECO, SENHA) VALUES (?,?, ?,?)");
+		statement.setInt(1, cpf);
+		statement.setString(2, nome);
+		statement.setString(3, endereco);
+		statement.setInt(4, senha);
+
+		statement.executeUpdate();
+		statement.close();
+		conexao.commit();
+
+	}
+
+	public ArrayList<String> lerDestinos(FirebirdConnection conexao) throws SQLException{
 		
+		ArrayList<String> listaDestinos = new ArrayList<String>();
+		
+		
+		// A necessidade deste lock eh para nao permitir atualizacao do registro que esta lendo ( leitura suja )
+		PreparedStatement  statement = conexao.prepareStatement("SELECT d.nome FROM DESTINOS d WITH LOCK");
+		
+		ResultSet resultado = statement.executeQuery();
+		
+		while(resultado.next()){
+			
+			listaDestinos.add(resultado.getString(1));
+			
+		}
+		
+		return listaDestinos;
 
 	}
 
-	public void lerDestinos(){
-
-
-	}
-
-	public void lerPoltronas(){
-
+	public void lerAssentosReservados(){
+		
+		
 
 	}
 
